1406bae2e34c6b77a375ff183525b42892b7aa3b
Shrey Dutta <0shreydutta0@gmail.com>
2022-11-04 11:29:43 +1000

    changed elapsed times in the comment in accord with average runs

--- simulations.m
+++ simulations.m
@@ -7,7 +7,7 @@ regime_type = 'AI'; % select either AI/Iso/BS/SZ
 durtn = 1;    % provide the duration of the simulation
 outdirpath = './output_dir'; % provide the output directory to save the simulation output;
 
-tic; % for getting runtime of the simulation ~ Elapsed time is 15.450183 seconds.
+tic; % for getting runtime of the simulation ~ Elapsed time is ~14.7 seconds.
 run_simulation(regime_type, durtn, outdirpath); % run the simulation;
 toc;
 
@@ -17,7 +17,7 @@ regime_type = 'Iso'; % select either AI/Iso/BS/SZ
 durtn = 1;    % provide the duration of the simulation
 outdirpath = './output_dir'; % provide the output directory to save the simulation output;
 
-tic; % for getting runtime of the simulation ~ Elapsed time is 15.334093 seconds.
+tic; % for getting runtime of the simulation ~ Elapsed time is ~13.97 seconds.
 run_simulation(regime_type, durtn, outdirpath); % run the simulation;
 toc;
 
@@ -27,7 +27,7 @@ regime_type = 'BS'; % select either AI/Iso/BS/SZ
 durtn = 50;    % provide the duration of the simulation
 outdirpath = './output_dir'; % provide the output directory to save the simulation output;
 
-tic; % for getting runtime of the simulation ~ Elapsed time is 722.761436 seconds.
+tic; % for getting runtime of the simulation ~ Elapsed time is ~675.41 seconds.
 run_simulation(regime_type, durtn, outdirpath); % run the simulation;
 toc;
 
@@ -37,7 +37,7 @@ regime_type = 'SZ'; % select either AI/Iso/BS/SZ
 durtn = 60;    % provide the duration of the simulation
 outdirpath = './output_dir'; % provide the output directory to save the simulation output;
 
-tic; % for getting runtime of the simulation ~ Elapsed time is 719.905010 seconds 
+tic; % for getting runtime of the simulation ~ Elapsed time is ~784.21 seconds 
 run_simulation(regime_type, durtn, outdirpath); % run the simulation;
 toc;
 
